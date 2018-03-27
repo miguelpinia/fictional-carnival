2be057b1245495aaec233632b97b15d9c453824e
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-03-26 22:04:18 -0600

    Agrega bootnode.sh

---
+++ bootnode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###########################################################################
+# Inicia una nodo de inicio con la imagen oficial de ethereum de alltools #
+###########################################################################
+
+# Paramos el contenedor si es que está ejecutándose
+docker stop ethereum-bootnode || true && \
+# Eliminamos el contenedor si existe
+docker rm ethereum-bootnode || true
+# Obtenemos la imagen del cliente de docker que queremos utilizar
+IMGNAME="ethereum/client-go:alltools-v1.7.3"
+DATA_ROOT=${DATA_ROOT:-$(pwd)}
+mkdir -p $DATA_ROOT/.bootnode
+# Genera el id del nodo.
+if [ ! -f $DATA_ROOT/.bootnode/boot.key ]; then
+    echo "No se encontró $DATA_ROOT/.bootnode/boot.key, generando..."
+    docker run --rm \
+           -v $DATA_ROOT/.bootnode:/opt/bootnode \
+           $IMGNAME bootnode --genkey /opt/bootnode/boot.key
+    echo "... Hecho!"
+fi
+
+# Inicializamos la red de ethereum
+
+[ ! "$(docker network ls | grep ethereum)" ] && docker network create ethereum
+docker run -d --name ethereum-bootnode \
+       -v $DATA_ROOT/.bootnode:/opt/bootnode \
+       -p 8545:8545 -p 30303:30303 \
+       --network ethereum \
+       $IMGNAME bootnode --nodekey /opt/bootnode/boot.key --verbosity=5 "$@"

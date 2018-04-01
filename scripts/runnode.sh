d2b17437e4ede533a5e26120e40a5251ae5286eb
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-04-01 16:46:40 -0500

    Mueve los scripts a la carpeta "scripts".

--- scripts/runnode.sh
+++ scripts/runnode.sh
@@ -22,26 +22,28 @@ if [[ ! -z $RPC_PORT ]]; then
     RPC_PORTMAP="-p $RPC_PORT:8545"
 fi
 # URL del nodo bootstrap.
-BOOTNODE_URL=${BOOTNODE_URL:-$(./getbootnodeurl.sh)}
-
-if [ ! -f $(pwd)/genesis.json ]; then
+# echo "Ejecutando"
+BOOTNODE_URL='enode://440a7f8ac2dc6c804e79cffa4613ea4187d7f6c1c18ddc4194c354ec6ed82bdd2eda4ba6d4d62600bcff505e7f8841042d354601e820ca81005b9189e75b1448@192.168.0.16:30301'
+echo $BOOTNODE_URL
+BOOTNODE_URL=${BOOTNODE_URL-$(./getbootnodeurl.sh)}
+echo $BOOTNODE_URL
+if [ ! -f "$(pwd)/genesis.json" ]; then
     echo "No se encontró el archivo genesis.json. Por favor ejecute 'genesis.sh'. Abortando..."
     exit
 fi
 
-if [ ! -d $DATA_ROOT/keystore ]; then
+if [ ! -d "$DATA_ROOT/keystore" ]; then
     echo "$DATA_ROOT/keystore no se encontró, ejecutando 'geth init'..."
     docker run --rm \
-           -v $DATA_ROOT:/root/.ethereum \
-           -v $(pwd)/genesis.json:/opt/genesis.json \
+           -v "$DATA_ROOT:/root/.ethereum" \
+           -v "$(pwd)/genesis.json:/opt/genesis.json" \
            $IMGNAME init /opt/genesis.json
     echo "...hecho!"
 fi
 echo "Ejecutando el nuevo contenedor $CONTAINER_NAME"
 docker run $DETACH_FLAG --name $CONTAINER_NAME \
-       $NETWORK \
-       -v $DATA_ROOT:/root/.ethereum \
-       -v $DATA_HASH:/root/.ethash \
-       -v $(pwd)/genesis.json:/opt/genesis.json \
+       -v "$DATA_ROOT:/root/.ethereum" \
+       -v "$DATA_HASH:/root/.ethash" \
+       -v "$(pwd)/genesis.json:/opt/genesis.json" \
        $RPC_PORTMAP \
-       $IMGNAME --bootnodes=$BOOTNODE_URL $RPC_ARG --cache=512 --verbosity=5 --maxpeers=10 ${@:2}
+       $IMGNAME --bootnodes=$BOOTNODE_URL $RPC_ARG --cache=512 --verbosity=5 --maxpeers=25 ${@:2}
